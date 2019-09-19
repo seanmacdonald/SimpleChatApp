ef29d37683fe4d8212624add00c8e3b0e071498b
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-19 14:40:11 -0700

    setup server to only accept one web socket connection and echo back any messages it receives

---
+++ client/main.go
@@ -0,0 +1,11 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting client...")
+	//TODO: Setup Dialer for connecting to the sever websocket 
+	
+}
--- server/main.go
+++ server/main.go
@@ -1,27 +1,37 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gorilla/websocket"
+	"net/http"
+	"runtime"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024, 
-	WriteBufferSize: 1024, 
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func connect(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-	conn, err :=  upgrader.Upgrade(w, r, nil)
+	fmt.Println("A new connection was made. There are now", runtime.NumGoroutine(), "goroutines running.")
+	
+	//Note that each http handler func starts a new goroutine and we want to limit
+	//this chat application between 2 users: the client and the server
+	if runtime.NumGoroutine() > 3 {
+		fmt.Println("Only one connection allowed at a time")
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
-	}	
+	}
 
 	for {
-		msgType, p, err :=  conn.ReadMessage() 
+		msgType, p, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -30,13 +40,19 @@ func connect(w http.ResponseWriter, r *http.Request) {
 			if msgType == 1 {
 				fmt.Println("message: ", string(p))
 			}
+
+			//echo the message back now
+			if err := conn.WriteMessage(1, p); err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
 	}
 }
 
-func main() { 
+func main() {
 
-	http.HandleFunc("/connect", connect)	
+	http.HandleFunc("/connect", connect)
 
 	fmt.Println("Starting server on port 8080...")
 	http.ListenAndServe(":8080", nil)
