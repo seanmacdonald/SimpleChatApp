46ba19d92cf42b1298e426abdac3650e97e78ab3
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-19 16:06:21 -0700

    server can now send and recieve messages

--- server/main.go
+++ server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"os"
 	"runtime"
 )
 
@@ -16,9 +18,10 @@ func connect(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
 	fmt.Println("A new connection was made. There are now", runtime.NumGoroutine(), "goroutines running.")
-	
+
 	//Note that each http handler func starts a new goroutine and we want to limit
-	//this chat application between 2 users: the client and the server
+	//this chat application between 2 users: the client and the server.
+	//Futhermore, when a websocket connection is terminated the goroutine will terminate.
 	if runtime.NumGoroutine() > 3 {
 		fmt.Println("Only one connection allowed at a time")
 		return
@@ -30,21 +33,54 @@ func connect(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//call read/write loop
+	message(conn)
+}
+
+func message(conn *websocket.Conn) {
+
+	read_chan := make(chan string)
+	input_chan := make(chan string)
+	defer close(read_chan)
+	defer close(input_chan)
+
+	go input(input_chan)
+	go readMessage(read_chan, conn)
+
+	for {
+		select {
+		case incomingMsg := <-read_chan:
+			fmt.Println(incomingMsg)
+		case outgoingMsg := <-input_chan:
+			fmt.Println("Send this: ", outgoingMsg)
+			if err := conn.WriteMessage(1, []byte(outgoingMsg)); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+		}
+	}
+}
+
+func input(c chan string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, _ := reader.ReadString('\n')
+		fmt.Println("in: ", text)
+		c <- text
+	}
+}
+
+func readMessage(c chan string, conn *websocket.Conn) {
 	for {
 		msgType, p, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			return
 		} else {
-			fmt.Println("message type: ", msgType)
 			if msgType == 1 {
-				fmt.Println("message: ", string(p))
-			}
-
-			//echo the message back now
-			if err := conn.WriteMessage(1, p); err != nil {
-				fmt.Println(err)
-				return
+				c <- string(p)
 			}
 		}
 	}
