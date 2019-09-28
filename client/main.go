d5fbeb115ba9bd6c479f28a2b26cac01eec07afb
Sean MacDonald <seandoylemacdonald@gmail.com>
2019-09-27 17:43:26 -0700

    Finished messaging between server and client so now communciation back and forth works well

--- client/main.go
+++ client/main.go
@@ -1,49 +1,77 @@
-package main 
+package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/url"
 	"log"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
 
 func connect(u url.URL) {
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	
+
 	if err != nil {
 		log.Println(err)
 	}
 	defer c.Close()
-	
 
-	message(c) 
+	message(c)
 }
 
 func message(c *websocket.Conn) {
 	read_chan := make(chan string)
+	write_chan := make(chan string)
 
 	go readMessage(read_chan, c)
+	go input(write_chan)
 
 	for {
 		select {
 		case incomingMsg := <-read_chan:
-			fmt.Println(incomingMsg)
+			fmt.Println("Server: ", incomingMsg)
+		case outgoingMsg := <-write_chan:
+			if err := c.WriteMessage(1, []byte(outgoingMsg)); err != nil {
+				fmt.Println(err)
+				return
+			}
+			if outgoingMsg == "bye" {
+				return
+			}
 		}
+	}
+}
+
+func input(input chan string) {
+	defer close(input)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if text == "bye" {
+			input <- text
+			return
+		}
+		input <- text
 	}
 }
 
 func readMessage(read chan string, c *websocket.Conn) {
 	defer close(read)
 
-	for{
+	for {
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println(err)
 		}
 		read <- string(message)
 	}
-	
+
 }
 
 func main() {
